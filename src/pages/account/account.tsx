0851bf9c4b19d4d1a741ad2229975f0fecc326c5
Тимур Марокко <69273257+proofblame@users.noreply.github.com>
2022-07-02 14:54:42 +0300

    account-page

---
+++ src/pages/account/account.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./account.module.scss";
+import { Container } from "../../components";
+
+export const Account = () => {
+  return (
+    <Container>
+      <section className={style.account}>Account</section>
+    </Container>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,6 @@ import { Main } from "./main/main";
 import { Brand } from "./brand/brand";
 import { Feedbacks } from "./feedbacks/feedbacks";
 import { Favorites } from "./favorites/favorites";
+import { Account } from "./account/account";
 
-export { Card, Main, Brand, Feedbacks, Favorites };
+export { Card, Main, Brand, Feedbacks, Favorites, Account };
